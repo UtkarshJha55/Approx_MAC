170f2b13e139434d48aac7aad4233145aceaf52e
Utkarsh Jha <64651522+UtkarshJha55@users.noreply.github.com>
2025-06-21 13:56:02 +0530

    Full adders in MAC

--- FA.v
+++ FA.v
@@ -5,4 +5,12 @@ module FA(carry, sum, a, b, c);
 	assign carry = (a&b)|(b&c)|(c&a);
 endmodule
 
-
+// Full adder with enable
+module FAenable(carry, sum, a, b, c, enable);
+    input a, b, c, enable;
+    output sum, carry;
+    
+    // Compute sum and carry based on enable
+    assign sum = (enable) ? (a ^ b ^ c) : sum;
+    assign carry = (enable) ? ((a & b) | (b & c) | (c & a)) : carry;
+endmodule
